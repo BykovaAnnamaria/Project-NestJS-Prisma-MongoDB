1f0881c670eddb40723527edbbefd0c28d26ff8f
Arseniy Bykov <bkvarseniy@gmail.com>
2023-03-24 11:53:32 +0200

    basic controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ParamsTokenFactory } from '@nestjs/core/pipes';
 import { getEnvironmentData } from 'worker_threads';
 import { CreateProductDto } from './dto/create-produst.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -16,8 +17,19 @@ export class ProductsController {
 
   @Post()
   create(@Body() CreateProductDto: CreateProductDto): string {
-    return `Title: ${CreateProductDto.title} Price: ${CreateProductDto.price}`
+    return `Title: ${CreateProductDto.title} Price: ${CreateProductDto.price}`;
   };
+
+@Put(':id')
+update(@Body() UpdateProductDto: UpdateProductDto, @Param('id') id: string) {
+  return 'Update' + id;
+};
+
+@Delete(':id')
+  remove(@Param('id') id: string) {
+    return 'remove' + id;
+  };
+
 };
 
 
