320eaa476928ac4e33a8544fe3130694476548eb
Arseniy Bykov <bkvarseniy@gmail.com>
2023-03-24 11:13:36 +0200

    Post request

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ParamsTokenFactory } from '@nestjs/core/pipes';
 import { getEnvironmentData } from 'worker_threads';
+import { CreateProductDto } from './dto/create-produst.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -11,6 +12,11 @@ export class ProductsController {
   @Get(':id')
   getOne(@Param('id') id: string): string {
     return 'getOne' + id;
+  };
+
+  @Post()
+  create(@Body() CreateProductDto: CreateProductDto): string {
+    return `Title: ${CreateProductDto.title} Price: ${CreateProductDto.price}`
   };
 };
 
