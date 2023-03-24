b6094c1b885bd78ccf14df832dc60404b12f3abd
Arseniy Bykov <bkvarseniy@gmail.com>
2023-03-24 13:18:59 +0200

    basic controller. statusCode

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,35 +1,35 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Redirect } from '@nestjs/common';
 import { ParamsTokenFactory } from '@nestjs/core/pipes';
 import { getEnvironmentData } from 'worker_threads';
 import { CreateProductDto } from './dto/create-produst.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
-export class ProductsController {
+export class ProductsController {  
   @Get()
+  @Redirect('https://google.com', 301)
   getAll(): string {
     return 'getAll';
-  };
+  }
+
   @Get(':id')
   getOne(@Param('id') id: string): string {
     return 'getOne' + id;
-  };
+  }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() CreateProductDto: CreateProductDto): string {
     return `Title: ${CreateProductDto.title} Price: ${CreateProductDto.price}`;
   };
 
-@Put(':id')
-update(@Body() UpdateProductDto: UpdateProductDto, @Param('id') id: string) {
-  return 'Update' + id;
-};
+  @Put(':id')
+  update(@Body() UpdateProductDto: UpdateProductDto, @Param('id') id: string) {
+    return 'Update ' + id;
+  }
 
-@Delete(':id')
+  @Delete(':id')
   remove(@Param('id') id: string) {
-    return 'remove' + id;
-  };
-
-};
-
-
+    return 'Remove ' + id;
+  }
+}
