de4fc4a8c7ea595d93234c077180a5ebd3f67e8e
Arseniy Bykov <bkvarseniy@gmail.com>
2023-03-27 18:21:17 +0300

    service creation

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,30 +1,35 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Redirect } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Header, HttpCode, HttpStatus, Param, Post, Put, Redirect } from '@nestjs/common';
 import { ParamsTokenFactory } from '@nestjs/core/pipes';
 import { getEnvironmentData } from 'worker_threads';
-import { CreateProductDto } from './dto/create-produst.dto';
+import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {  
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {
+  }
+
   @Get()
-  @Redirect('https://google.com', 301)
-  getAll(): string {
-    return 'getAll';
+  // @Redirect('https://google.com', 301)
+  getAll() {
+    return this.productsService.getAll()
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string): string {
-    return 'getOne' + id;
+  getOne(@Param('id') id: string) {
+    return this.productsService.getById(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  create(@Body() CreateProductDto: CreateProductDto): string {
-    return `Title: ${CreateProductDto.title} Price: ${CreateProductDto.price}`;
-  };
+  @Header('Products', 'none')
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
 
   @Put(':id')
-  update(@Body() UpdateProductDto: UpdateProductDto, @Param('id') id: string) {
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
     return 'Update ' + id;
   }
 
