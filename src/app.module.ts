e9d7c8183839a2992639403e0fd5264fb95937ab
Arseniy Bykov <bkvarseniy@gmail.com>
2023-04-19 18:57:14 +0300

    prisma mongo with mistakes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma.module';
 import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
 import { ProductsService } from './products/products.service';
 
 @Module({
-  imports: [],
-  controllers: [ProductsController],
-  providers: [ProductsService],
+  imports: [ProductsModule, PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
